82265d85230f7b226f25b6b9b031cf7498676f0d
Christian Barahona <christian.barahona@globant.com>
2022-07-13 19:16:39 -0500

    Agregando ejecucion de pruebas unitarias por consola

--- src/test/java/com/globantx/cardinalevaluator/CucumberConfig.java
+++ src/test/java/com/globantx/cardinalevaluator/CucumberConfig.java
@@ -11,7 +11,7 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "html:build/reports/cucumber/report.html")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.globant.courier")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.globantx.cardinalevaluator")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "not @Disabled")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(key = Constants.JUNIT_PLATFORM_NAMING_STRATEGY_PROPERTY_NAME, value = "long")
